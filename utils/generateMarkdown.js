2afa52bc3e5d052d842771de2f051bea32fee790
Nhi Danis <a.nhi1001@gmail.com>
2020-12-01 22:06:52 -0800

    commit generateMarkdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,53 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+# ${data.title}
+
+## ${data.badge}
+
+## Description:
+${data.description}
+
+
+
+## Table of Contents:
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributor](#contributor)
+* [License](#license)
+* [Test](#test)
+* [Questions](#question)
+
+## Screen Shot
+
+## Installation:
+${data.install}
+
+
+
+## Usage: 
+${data.usage}
+
+
+
+## License:
+${data.license}
+
+
+## Contributor:
+* [Github](https://github.com/${data.usename})
+
+
+## Tests:
+${data.test}
+
+## Question:
+Any Questions please contact me At:
+* [Github Profile](https://github.com/${data.usename})
+* [Email](${data.email})
+
 
 `;
 }
